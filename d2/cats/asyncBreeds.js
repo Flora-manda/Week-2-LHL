d7456e65281a677d14d5176208613a518ed3724c
Flora-manda <flora.uwadiegwu@gmail.com>
2020-03-11 13:49:26 +0000

    Cat breed exercise updated

--- d2/cats/asyncBreeds.js
+++ d2/cats/asyncBreeds.js
@@ -7,7 +7,12 @@ const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     // CHANGE: Pass data into callback instead of returning it directly
     console.log("In readFile's Callback: it has the data.");
-    if (!error) functionToRunWhenThingsAreDone(data);
+    if (!error) {
+      functionToRunWhenThingsAreDone(data);
+    }
+    else {
+      return functionToRunWhenThingsAreDone(undefined);
+    }
   });
 };
 
@@ -18,3 +23,5 @@ const printOutCatBreed = breed => {
 
 // CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
 breedDetailsFromFile('Bombay', printOutCatBreed);
+
+module.exports = breedDetailsFromFile;
